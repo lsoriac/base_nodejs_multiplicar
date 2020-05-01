8b8278bbb8990b487f0fb16ea84a0dce4223471e
Santiago Soria <52500411+lsoriac@users.noreply.github.com>
2020-04-30 23:21:28 -0500

    Añadir comentarios en el código

--- config/yargs.js
+++ config/yargs.js
@@ -1,14 +1,18 @@
 ////CONFIGURACION
+//agrupar opciones en común en la variable opc
 let opc = {
-    base: {
-        demand: true,
-        alias: 'b'
-    },
-    limite: {
-        alias: 'l',
-        default: 10
+        base: {
+            //es un valor requerido
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            //valor por defecto en caso de no enviarlo
+            default: 10
+        }
     }
-}
+    //forma optimizada de añadir comandos
 const argv = require('yargs')
     .command('listar', 'Imprime en consola la tabla de multiplicar', opc)
     .command('crear', 'Crea un archivo de la tabla multiplicar', opc)
